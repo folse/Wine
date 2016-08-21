064e770268469a8b63e63e142bc3268520afc843
folse <folsee@gmail.com>
2016-08-22 02:47:06 +0800

    update xls to xlsx

--- inventory_excel.py
+++ inventory_excel.py
@@ -1,33 +1,16 @@
 #!/usr/bin/python 
-#coding=utf-8
+# -*- coding: UTF-8 -*-
 import os
-import json
 import time
 import datetime
-import urllib
-import urllib2
-from xlwt import Workbook
-import xlrd
+import xlsxwriter
 import psycopg2
 
 import sys  
 reload(sys)
+sys.setdefaultencoding('utf8')
 
-inventory_file_name = 'inventory_%s.xls' % time.strftime('%Y-%m-%d',time.localtime(time.time()))
-
-def write_30days_period(wine_id,store_id,read_sheet):
-	for i in xrange(1,31):
-		j = 3
-		while (j > 0):
-			day_period = get_day(i) + '-' +str(j)
-			j-=1
-			column = (i-1)*3+(3-j)
-
-			cursor.execute("select inventory from inventory where wine_id = %s and store_id = %s and day_period = %s", (wine_id, store_id, day_period))
-			result = cursor.fetchone()
-			if result != None:
-				inventory = result[0]
-				sheet.write(read_sheet.nrows,1+column,inventory)
+inventory_file_name = 'inventory_%s.xlsx' % time.strftime('%Y-%m-%d',time.localtime(time.time()))
 
 def get_last_day_period():
 
@@ -46,75 +29,57 @@ def remove_old_file():
 	if os.path.isfile(inventory_file_name): 
 		os.remove(inventory_file_name)
 
-def write_30days_title(read_sheet):
-	hour_period_array = ['10:00', '14:00', '22:00']
-	for i in xrange(1,31):
-		j = 3
-		while (j > 0):
-			j-=1
-			day_period = get_day(i)[5:] + ' ' + hour_period_array[j]
-			column = (i-1)*3+(3-j)
-
-			sheet.write(read_sheet.nrows+2,1+column,day_period)
-
 if __name__ == '__main__':
 
 	remove_old_file()
 
-	wine_subcategory = u'Rött vin'
+	book = xlsxwriter.Workbook(inventory_file_name)
+	sheet = book.add_worksheet()
 
-	global sheet
-
-	book = Workbook(encoding = 'utf-8')
-	sheet = book.add_sheet(wine_subcategory)
-	book.save(inventory_file_name)
+	row = -3
+	col = 0
 
 	conn = psycopg2.connect(database="wine", user="postgres", password="makeFuture", host="localhost", port="5432")
 	cursor = conn.cursor()
 	cursor.execute("select * from store")
 	store_array = cursor.fetchall()
-	for i in range(len(store_array)):
-
-		store = store_array[i]
+	for store in store_array:
 
 		if store != None:
 
 			store_id = str(store[0])
 			store_name = str(store[1])
 
-			store_info = 'Store Id: ' + store_id + ', Store Name: ' + store_name
-
 			print store_id
 
-			read_book = xlrd.open_workbook(inventory_file_name)
-			read_sheet = read_book.sheet_by_index(0)
-			sheet.write(read_sheet.nrows,0,'')
-			sheet.write(read_sheet.nrows+1,0,store_info)
-			sheet.write(read_sheet.nrows+2,0,'Product Name')
-			sheet.write(read_sheet.nrows+2,1,'Product Id')
-			write_30days_title(read_sheet)
-			book.save(inventory_file_name)
+			row +=3
+
+			store_info = 'Store Id: ' + store_id + ', Store Name: ' + store_name
+			
+			sheet.write(row,col,store_info)
+			sheet.write(row+1,col,'Product Name')
+			sheet.write(row+1,col+1,'Product Id')
+			sheet.write(row+1,col+2,get_day(1)[5:] + ' ' + '22:00')
 
 			cursor.execute("select * from inventory where store_id = %s and day_period = %s", (store_id, get_last_day_period()))
-			wine_array = cursor.fetchall()
+			inventory_array = cursor.fetchall()
 
-			for i in range(len(wine_array)):
+			for inventory in inventory_array:
 
-				wine = wine_array[i]
-				wine_id = wine[1]
-				wine_name = wine[6]
-				wine_number = wine[7]
+				wine_id = inventory[1]
+				wine_name = inventory[6]
+				wine_number = inventory[7]
+				wine_inventory = inventory[3]
 
-				read_book = xlrd.open_workbook(inventory_file_name)
-				read_sheet = read_book.sheet_by_index(0)
-				sheet.write(read_sheet.nrows,0,wine_name)
-				sheet.write(read_sheet.nrows,1,wine_number)
+				sheet.write(row+2,col,wine_name)
+				sheet.write(row+2,col+1,wine_number)
+				sheet.write(row+2,col+2,wine_inventory)
 
-				write_30days_period(wine_id, store_id, read_sheet)
+				row +=1
 
-				book.save(inventory_file_name)
-	
+	book.close()
 	cursor.close()
 	conn.close()
+	
 
 
--- wine_store_excel.py
+++ wine_store_excel.py
@@ -4,16 +4,14 @@ import os
 import json
 import time
 import datetime
-import urllib
-import urllib2
-from xlwt import Workbook
-import xlrd
+import xlsxwriter
 import psycopg2
 
 import sys  
 reload(sys)
+sys.setdefaultencoding('utf8')
 
-wine_file_name = 'wine_%s.xls' % time.strftime('%Y-%m-%d',time.localtime(time.time()))
+wine_file_name = 'wine_%s.xlsx' % time.strftime('%Y-%m-%d',time.localtime(time.time()))
 
 def remove_old_file():
 	if os.path.isfile(wine_file_name): 
@@ -24,7 +22,6 @@ def get_last_day_period():
 	return get_day(1) + '-3'
 
 def get_day(fix_day):
-
 	now_time = datetime.datetime.now()
 	last_time = now_time + datetime.timedelta(days=-fix_day)
 
@@ -36,21 +33,18 @@ if __name__ == '__main__':
 
 	remove_old_file()
 
-	wine_subcategory = u'Rött vin'
-
 	update_time_period = get_last_day_period()
 
-	book = Workbook(encoding = 'utf-8')
-	sheet = book.add_sheet(wine_subcategory)
-	book.save(wine_file_name)
+	book = xlsxwriter.Workbook(wine_file_name)
+	sheet = book.add_worksheet()
+
+	row = -3
 
 	conn = psycopg2.connect(database="wine", user="postgres", password="makeFuture", host="localhost", port="5432")
 	cursor = conn.cursor()
 	cursor.execute("select * from store")
 	store_array = cursor.fetchall()
-	for i in range(len(store_array)):
-
-		store = store_array[i]
+	for store in store_array:
 
 		if store != None:
 
@@ -61,45 +55,41 @@ if __name__ == '__main__':
 
 			print store_id
 
-			read_book = xlrd.open_workbook(wine_file_name)
-			read_sheet = read_book.sheet_by_index(0)
-			sheet.write(read_sheet.nrows,0,'')
-			sheet.write(read_sheet.nrows+1,0,store_info)
-			sheet.write(read_sheet.nrows+2,0,'Product Name')
-			sheet.write(read_sheet.nrows+2,1,'Product Id')
-			sheet.write(read_sheet.nrows+2,2,'Säljstart')
-			sheet.write(read_sheet.nrows+2,3,'Alkoholhalt')
-			sheet.write(read_sheet.nrows+2,4,'Färg')
-			sheet.write(read_sheet.nrows+2,5,'Doft')
-			sheet.write(read_sheet.nrows+2,6,'Råvaror')
-			sheet.write(read_sheet.nrows+2,7,'Sockerhalt')
-			sheet.write(read_sheet.nrows+2,8,'Producent')
-			sheet.write(read_sheet.nrows+2,9,'Leverantör')
-			book.save(wine_file_name)
+			row += 3
+
+			sheet.write(row,0,store_info)
+			sheet.write(row+1,0,'Product Name')
+			sheet.write(row+1,1,'Product Id')
+			sheet.write(row+1,2,'Säljstart')
+			sheet.write(row+1,3,'Alkoholhalt')
+			sheet.write(row+1,4,'Färg')
+			sheet.write(row+1,5,'Doft')
+			sheet.write(row+1,6,'Råvaror')
+			sheet.write(row+1,7,'Sockerhalt')
+			sheet.write(row+1,8,'Producent')
+			sheet.write(row+1,9,'Leverantör')
 
 			cursor.execute("select wine_id from inventory where store_id = %s and day_period = %s", (store_id, update_time_period))
 			wine_id_array = cursor.fetchall()
-			for i in range(len(wine_id_array)):
+			for wine_id in wine_id_array:
 
-				wine_id = wine_id_array[i]
 				cursor.execute("select * from wine where id = %s", wine_id)
 				wine = cursor.fetchone()
 
-				read_book = xlrd.open_workbook(wine_file_name)
-				read_sheet = read_book.sheet_by_index(0)
-				sheet.write(read_sheet.nrows,0,wine[1])
-				sheet.write(read_sheet.nrows,1,wine[11])
-				sheet.write(read_sheet.nrows,2,wine[2])
-				sheet.write(read_sheet.nrows,3,wine[3])
-				sheet.write(read_sheet.nrows,4,wine[4])
-				sheet.write(read_sheet.nrows,5,wine[5])
-				sheet.write(read_sheet.nrows,6,wine[6])
-				sheet.write(read_sheet.nrows,7,wine[7])
-				sheet.write(read_sheet.nrows,8,wine[8])
-				sheet.write(read_sheet.nrows,9,wine[9])
-				book.save(wine_file_name)
-	
+				sheet.write(row+2,0,wine[1])
+				sheet.write(row+2,1,wine[11])
+				sheet.write(row+2,2,wine[2])
+				sheet.write(row+2,3,wine[3])
+				sheet.write(row+2,4,wine[4])
+				sheet.write(row+2,5,wine[5])
+				sheet.write(row+2,6,wine[6])
+				sheet.write(row+2,7,wine[7])
+				sheet.write(row+2,8,wine[8])
+				sheet.write(row+2,9,wine[9])
+
+				row += 1
+
+	book.close()
 	cursor.close()
 	conn.close()
-
 
